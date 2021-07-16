fdf61be7bfc29c5bf1f496e5eb6079ec97e6613d
Joshua <josu.arambula@dexcom.com>
2021-07-15 19:41:45 -0500

    Models and Networking are created

--- Pokedex/Pokedex/Controllers/ViewController.swift
+++ Pokedex/Pokedex/Controllers/ViewController.swift
@@ -12,6 +12,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+
+      let network = NetworkManager()
+      network.getAllPokemons()
+
+      // test
+      network.getPokemon(withName: "bulbasaur")
+
     }
 }
 
