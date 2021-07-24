c69389f65b86a8a7ebbf61139fdd69faa4ea8019
Fernando Martin Garcia Del Angel <mxg0820@dexcom.com>
2021-07-24 11:53:39 -0700

    Add Project and SplashScreen

---
+++ Pokedex/Pods/Target Support Files/Pods-PokedexTests/Pods-PokedexTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PokedexTests : NSObject
+@end
+@implementation PodsDummy_Pods_PokedexTests
+@end
---
+++ Pokedex/Pokedex/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by Martin García on 7/24/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
